ff491acf1650175b4b436df09f5348791a0e9f99
Kamaro Lambert <kamaroly@gmail.com>
2015-07-27 00:37:31 +0200

    Added softDelete columns to models

--- app/Models/FeeTransaction.php
+++ app/Models/FeeTransaction.php
@@ -1,9 +1,8 @@
 <?php namespace App\Models;
 
-use DB;
-use App\Models\User;
 use App\Models\Student;
-
+use App\Models\User;
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class FeeTransaction extends Model {
@@ -11,41 +10,38 @@ class FeeTransaction extends Model {
 	/** @var string Table that is associated to this model */
 	public $table = 'fee_transactions';
 
-	protected $fillable =[
-						'debit',
-						'credit',
-						'balance',
-						'description',
-						'payslip_number',
-						'receipt_number',
-						'date',
-						'student_id',
-						'done_by',
-						];
+	protected $fillable = [
+		'debit',
+		'credit',
+		'balance',
+		'description',
+		'payslip_number',
+		'receipt_number',
+		'date',
+		'student_id',
+		'done_by',
+	];
 
 	/**
 	 * Relationship with the student
 	 * @return this
 	 */
-	public function student()
-	{
+	public function student() {
 		return $this->belongsTo('App\Models\Student');
 	}
 
 	/**
 	 * Relationship with the user Table
-	 * 
+	 *
 	 * @return this
 	 */
-	public function doneBy()
-	{
-		return $this->belongsTo('App\Models\User','done_by');
+	public function doneBy() {
+		return $this->belongsTo('App\Models\User', 'done_by');
 	}
 
-	public function getChartData()
-	{
-	 return	DB::table($this->table)
-                 ->select(DB::raw('LEFT(date,7) as month, sum(credit) as credit,sum(debit) as debit'))
-                 ->groupBy(DB::raw('LEFT(date,7)'));
+	public function getChartData() {
+		return DB::table($this->table)
+			->select(DB::raw('LEFT(date,7) as month, sum(credit) as credit,sum(debit) as debit'))
+			->groupBy(DB::raw('LEFT(date,7)'));
 	}
 }
