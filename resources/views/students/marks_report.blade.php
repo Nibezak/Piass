5e8a60e95ba0420ccb39b28c1386cc09de6a3e05
Kamaro Lambert <kamaroly@gmail.com>
2015-10-18 16:21:31 +0200

    Added marks summary to the report

--- resources/views/students/marks_report.blade.php
+++ resources/views/students/marks_report.blade.php
@@ -11,5 +11,12 @@
 	</thead>
   <tbody>
     @each('students.mark_item', $student->marks, 'module', 'student.empty')
+    <tr>
+      <th colspan="2"> General Average: </th>
+      <th> {!! $student->marks->sum('marks') / $student->marks->count();  !!} </th>
+      <th> Total credits: </th>
+      <th> {!! $student->marks->sum('module_credits') !!} </th>
+      <th> {!! $student->marks->sum('module_credits') * $student->marks->sum('marks') !!} </th>
+    </tr>
   </tbody>
 </table>
